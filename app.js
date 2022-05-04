8a583c29e573efb2f5abf574617e15083c14d4aa
NicolasDev153 <nikaberidze27@gmail.com>
2022-05-04 16:03:34 +0400

    added more settings for screen adjustment

--- app.js
+++ app.js
@@ -22,6 +22,17 @@ $(document).ready(() => {
             dots: false,
             loop: true,
             autoplay: false,
-            autoplayHoverPause: true
+            autoplayHoverPause: true,
+            responsive: {
+                500: {
+                    items: 3
+                },
+                1280: {
+                    items: 4
+                },
+                1600: {
+                    items: 6
+                }
+            }
         })
 })
